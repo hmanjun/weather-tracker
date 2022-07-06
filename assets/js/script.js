f6b158a1894e52b7469fda2a6ae8afd1117b0db2
hmanjun <harshith.hm03@gmail.com>
2022-07-06 00:13:30 -0700

    Add weather icon elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,7 +36,11 @@ function getWeatherData(name){
 
 function displayCurrentTemp(data){
     var currTempCont = $("<div>").addClass("container d-flex flex-column")
+    var iconUrl = "http://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + "@2x.png"
+    var img = $("<img>").attr("src", iconUrl)
+    img.attr("alt", "Weather Icon")
     var title = $("<h2>").text(data.city["name"] +" "+ moment(data.list[0].dt_txt).format("[(]D[/]MM[/]YYYY[)]"))
+    title.append(img)
     var temp = $("<h5>").text("Temp: " + data.list[0].main["temp"] + " F")
     var winds = $("<h5>").text("Wind: " + data.list[0].wind.speed + " MPH")
     var humid = $("<h5>").text("Humidity: " + data.list[0].main["temp"] + "%")
@@ -47,17 +51,27 @@ function displayCurrentTemp(data){
 }
 
 function displayForecastCards(data){
-    var title = $("<h3>").text("5-Day Forecast:")
+    var title = $("<h3>").text("5-Day Forecast:").addClass("container")
+    var cardCont = $("<div>").addClass("container d-flex flex-row")
     infoCont.append(title)
+    
     for(var i = 1; i <= 5; i++){
         var card = $("<div>").addClass("card").css("width", "18rem")
         var date = $("<h4>").text(moment().add(1,'days').format("[(]D[/]MM[/]YYYY[)]"))
+        var iconUrl = "http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + "@2x.png"
+        //var img = $("<img>").attr("src", iconUrl)
+        var img = $("<img>").attr({
+            src: iconUrl,
+            width: 70,
+            height: 70
+        })
         var temp = $("<h5>").text("Temp: " + data.list[i].main["temp"] + " F")
         var winds = $("<h5>").text("Wind: " + data.list[i].wind.speed + " MPH")
         var humid = $("<h5>").text("Humidity: " + data.list[i].main["temp"] + "%")
-        card.append(date, temp, winds, humid)
-        infoCont.append(card)
+        card.append(date, img, temp, winds, humid)
+        cardCont.append(card)
     }
+    infoCont.append(cardCont)
     
 }
 //test url https://api.openweathermap.org/data/2.5/forecast?q=sunnyvale&appid=$db2c49bf1420cd2c3d40a1df5c2e744c
