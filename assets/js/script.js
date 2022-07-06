4db9a088d3217af4bd7b00b4ef646007fb5b2164
hmanjun <harshith.hm03@gmail.com>
2022-07-06 14:52:02 -0700

    Switch to use 'one-call' api for weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,16 @@ var infoCont = $(".info-container")
 var lon
 var lat
 var uv
+var colorCode
+
+//console.log(moment().format("ddd, hA"))
+//console.log(moment().unix())
 
 $(document).on("click", "#search-btn", function(event){
     var cityName = $(this).siblings("#city-search-val").val()
-    getWeatherData(cityName)
+    //getWeatherData(cityName)
+    getGeoLocation(cityName)
+    addToHistory(cityName)
 
 })
 
@@ -14,7 +20,6 @@ function addToHistory(name){
     if(searchHistory.includes(name)){
         return
     }
-    console.log("reached funct")
     searchHistory.unshift(name)
     var histCont = $("#history-cont")
     var cityBtn = $("<button>").addClass("btn btn-secondary text-light my-2").text(name)
@@ -22,23 +27,24 @@ function addToHistory(name){
 }
 
 function getWeatherData(name){
-    var getUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + name + "&units=imperial&appid=410463b3935acea56c8171825dbb4440"
+    //var getUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + name + "&units=imperial&appid=410463b3935acea56c8171825dbb4440"
     //var getUrl = "https://api.openweathermap.org/data/2.5/onecall?q="+name+"&exclude=hourly&units=imperial&appid=410463b3935acea56c8171825dbb4440"
     //var getUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=410463b3935acea56c8171825dbb4440"
-    getGeoLocation(name)
+    var getUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=3279c108edd62f03f66f2e92780c6761"
     
     $.ajax({
         url: getUrl,
         method: "GET"
     })
         .then(function(response){
-            console.log(lon,lat)
+            //console.log(lon,lat)
             console.log(response)
             //getUV(name)
-            addToHistory(name)
-            console.log(response.list[0].main["temp"])
-            console.log(response.list[0].dt_txt)
-            displayCurrentTemp(response)
+            //addToHistory(name)
+            //console.log(response.list[0].main["temp"])
+            //console.log(response.list[0].dt_txt)
+
+            displayCurrentTemp(response, name)
             displayForecastCards(response)
         })
 }
@@ -49,15 +55,26 @@ function getGeoLocation(name){
         method: "GET"
     })
         .then(function(response){
-            console.log("geo loc", response)
+            //console.log("geo loc", response)
             lon = response[0].lon
             lat = response[0].lat
-            getUV()
+            //getUV()
+            //displayCurrentTemp(response)
+            getWeatherData(name)
         })
 }
 
+function colorCodeUV(index){
+    if(index >= 8){
+        colorCode = "bg-danger"
+    } else if(index >= 3){
+        colorCode = "bg-warning"
+    } else {
+        colorCode = "bg-success"
+    }
+}
+
 function getUV(){
-    console.log(lat, lon)
     var getUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=3279c108edd62f03f66f2e92780c6761"
     $.ajax({
         url: getUrl,
@@ -66,7 +83,6 @@ function getUV(){
         .then(function(response){
             console.log("UV data", response)
             uv = response.current.uvi
-            var colorCode
             if(uv >= 8){
                 colorCode = "bg-danger"
             } else if(uv >= 3){
@@ -81,20 +97,23 @@ function getUV(){
         })
 }
 
-function displayCurrentTemp(data){
+function displayCurrentTemp(data,name){
     var currTempCont = $("<div>").addClass("container d-flex flex-column curr-temp-cont")
-    var iconUrl = "http://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + "@2x.png"
+    var iconUrl = "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2x.png"
     var img = $("<img>").attr("src", iconUrl)
     img.attr("alt", "Weather Icon")
-    var title = $("<h2>").text(data.city["name"] +" "+ moment(data.list[0].dt_txt).format("[(]D[/]MM[/]YYYY[)]"))
+    //console.log(data.current.dt, typeof(data.current.dt, String(data.current.dt), typeof(String(data.current.dt))))
+    var title = $("<h2>").text(name +" "+ moment().format("[(]M[/]DD[/]YYYY[)]"))
     title.append(img)
-    var temp = $("<h5>").text("Temp: " + data.list[0].main["temp"] + " F")
-    var winds = $("<h5>").text("Wind: " + data.list[0].wind.speed + " MPH")
-    var humid = $("<h5>").text("Humidity: " + data.list[0].main["temp"] + "%")
-    currTempCont.append(title, temp, winds, humid)
+    var temp = $("<h5>").text("Temp: " + data.current.temp + " F")
+    var winds = $("<h5>").text("Wind: " + data.current.wind_speed + " MPH")
+    var humid = $("<h5>").text("Humidity: " + data.current.humidity + "%")
+    var uvTitle = $("<h5>").text("UV Index: ").addClass("d-flex flex-row")
+    colorCodeUV(data.current.uvi)
+    uvTitle.append($("<h5>").text(" "+ data.current.uvi).addClass(colorCode))
+    currTempCont.append(title, temp, winds, humid, uvTitle)
     infoCont.append(currTempCont)
-    console.log(data.city["name"] + moment(data.list[0].dt_txt).format("[(]D[/]MM[/]YYYY[)]"))
-    //data.list[0].weather[0].icon
+    //console.log(data.city["name"] + moment(data.list[0].dt_txt).format("[(]D[/]MM[/]YYYY[)]"))
 }
 
 function displayForecastCards(data){
@@ -102,19 +121,19 @@ function displayForecastCards(data){
     var cardCont = $("<div>").addClass("container d-flex flex-row")
     infoCont.append(title)
     
-    for(var i = 1; i <= 5; i++){
+    for(var i = 0; i < 5; i++){
         var card = $("<div>").addClass("card").css("width", "18rem")
-        var date = $("<h4>").text(moment().add(1,'days').format("[(]D[/]MM[/]YYYY[)]"))
-        var iconUrl = "http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + "@2x.png"
+        var date = $("<h4>").text(moment().add(i+1,'days').format("[(]M[/]DD[/]YYYY[)]"))
+        var iconUrl = "http://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + "@2x.png"
         //var img = $("<img>").attr("src", iconUrl)
         var img = $("<img>").attr({
             src: iconUrl,
             width: 70,
             height: 70
         })
-        var temp = $("<h5>").text("Temp: " + data.list[i].main["temp"] + " F")
-        var winds = $("<h5>").text("Wind: " + data.list[i].wind.speed + " MPH")
-        var humid = $("<h5>").text("Humidity: " + data.list[i].main["temp"] + "%")
+        var temp = $("<h5>").text("Temp: " + data.daily[i].temp["day"] + " F")
+        var winds = $("<h5>").text("Wind: " + data.daily[i].wind_speed + " MPH")
+        var humid = $("<h5>").text("Humidity: " + data.daily[i].humidity + "%")
         card.append(date, img, temp, winds, humid)
         cardCont.append(card)
     }
