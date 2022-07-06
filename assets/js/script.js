a5586784bb548fd61c434a0819e15f97dd364e50
hmanjun <harshith.hm03@gmail.com>
2022-07-05 19:10:57 -0700

    Add 5 day forecast card dynamic generation

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,6 +30,7 @@ function getWeatherData(name){
             //console.log(response.list[0].main["temp"])
             //console.log(response.list[0].dt_txt)
             displayCurrentTemp(response)
+            displayForecastCards(response)
         })
 }
 
@@ -43,6 +44,21 @@ function displayCurrentTemp(data){
     infoCont.append(currTempCont)
     console.log(data.city["name"] + moment(data.list[0].dt_txt).format("[(]D[/]MM[/]YYYY[)]"))
     //data.list[0].weather[0].icon
+}
+
+function displayForecastCards(data){
+    var title = $("<h3>").text("5-Day Forecast:")
+    infoCont.append(title)
+    for(var i = 1; i <= 5; i++){
+        var card = $("<div>").addClass("card").css("width", "18rem")
+        var date = $("<h4>").text(moment().add(1,'days').format("[(]D[/]MM[/]YYYY[)]"))
+        var temp = $("<h5>").text("Temp: " + data.list[i].main["temp"] + " F")
+        var winds = $("<h5>").text("Wind: " + data.list[i].wind.speed + " MPH")
+        var humid = $("<h5>").text("Humidity: " + data.list[i].main["temp"] + "%")
+        card.append(date, temp, winds, humid)
+        infoCont.append(card)
+    }
+    
 }
 //test url https://api.openweathermap.org/data/2.5/forecast?q=sunnyvale&appid=$db2c49bf1420cd2c3d40a1df5c2e744c
 
